49773f852960a2350a808fdef83e0efd36741db2
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2023-06-14 13:38:23 -0300

    primer comit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div>
+        <header>
+            Barra de Navegacion
+        </header>
+        <main>
+            <section> Seccion 1 </section>
+            <section> Seccion 2 </section>
+            <section> Seccion 3 </section>
+            <section> Seccion 4 </section>
+            <section> Seccion 5 </section>
+        </main>
+    </div>
+  )
+}
